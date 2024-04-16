c7ec5aca33561e3c522cf3dd9efa917ce26a585f
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-16 15:52:22 +0100

    Basic

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,18 +14,32 @@ class BasicAuth(Auth):
             self, authorization_header: str) -> str:
         """Extracts the base64 part of the authorization header."""
         if (authorization_header is None or
-            not isinstance(authorization_header, str) or
-            not authorization_header.startswith('Basic ')):
+                not isinstance(authorization_header, str) or
+                not authorization_header.startswith('Basic ')):
             return None
         else:
             return authorization_header[6:]
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
         """Decodes the base64 part of the authorization header."""
         if (base64_authorization_header is None or
-            not isinstance(base64_authorization_header, str)):
+                not isinstance(base64_authorization_header, str)):
             return None
         try:
-            return base64.b64decode(base64_authorization_header).decode('utf-8')
+            return base64.b64decode(
+                base64_authorization_header).decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """Extracts the user credentials from the
+        decoded base64 authorization header.
+        """
+        if (decoded_base64_authorization_header is None or
+                not isinstance(decoded_base64_authorization_header, str) or
+                ':' not in decoded_base64_authorization_header):
+            return (None, None)
+        else:
+            return tuple(decoded_base64_authorization_header.split(':', 1))
