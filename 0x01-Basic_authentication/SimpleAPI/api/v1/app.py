ee9b7978a4746b7e51df3c449df57d0c57cb698d
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-16 12:11:41 +0100

    Basic

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -27,6 +27,7 @@ def unauthorized(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """
@@ -37,5 +38,5 @@ def forbidden(error) -> str:
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5003")
+    port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
