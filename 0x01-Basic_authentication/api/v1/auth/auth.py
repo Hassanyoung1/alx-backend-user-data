a7aab262ca69a4e87869a622720538ba5af88126
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-16 14:03:35 +0100

    Request

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -54,14 +54,11 @@ class Auth:
         Returns:
             str: The Authorization header value if present, otherwise None.
         """
-        if request is None or 'Authorization' not in request.headers:
+        if request is None:
             return None
-
-        authorization_header = request.headers.get('Authorization')
-        if not authorization_header:
+        if 'Authorization' not in request.headers:
             return None
-
-        return authorization_header
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
