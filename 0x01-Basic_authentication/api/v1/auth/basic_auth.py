c96d506ffa8423a9511ef08c8e9444c562470528
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-16 15:00:45 +0100

    Basic

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+This module contains the basic authentication class.
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic authentication class."""
+    pass
