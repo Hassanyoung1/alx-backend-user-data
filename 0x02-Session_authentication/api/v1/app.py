797797c981a1653af4dea405b95346181e780c70
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-20 02:16:45 +0100

    Before

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,11 +34,10 @@ def before_request():
         return
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if request.path not in excluded_paths:
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:
-                abort(401)
             auth_header = auth.authorization_header(request)
             session_cookie = auth.session_cookie(request)
             if auth_header is None and session_cookie is None:
