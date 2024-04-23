1e1bd974374aaffc7930e9f176f65642bb7d3308
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-23 14:37:09 +0100

    user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -40,6 +40,21 @@ def users() -> str:
     return jsonify({"email": email, "message": "user created"})
 
 
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """
+    login route
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
+
+
 if __name__ == "__main__":
     """
     Main function
