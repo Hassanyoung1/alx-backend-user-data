fefbfd813aac847b6829278ada7fcdec57797a9f
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-23 13:09:21 +0100

    user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -61,8 +61,11 @@ class DB:
         """
         try:
             user = self.find_user_by(id=user_id)
-            for key, value in kwargs.items():
-                setattr(user, key, value)
-            self._session.commit()
         except NoResultFound:
-            raise ValueError("No user found with the provided id")
+            raise ValueError()
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError
+        self._session.commit()
