bfb8675aa0be4b2dae069f6498bcd1c456abf095
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-23 12:11:30 +0100

    user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
-"""User model for SQLAlchemy."""
-
+"""
+Declare a SQLAlchemy model named 'User' corresponding to a
+database table named "users"
+"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+)
 
 Base = declarative_base()
 
 
 class User(Base):
-    """Data model for a user.
     """
-    __tablename__ = 'users'
+    Definition of class User
+    """
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-<<<<<<< HEAD
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_token = Column(String(250), nullable=True)
+    session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-=======
-    email = Column(String, nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_token = Column(String(250), nullable=True)
-    reset_token = Column(String(250250250250250250250250250250250250250250250250250250250250250250250250250250), nullable=True)
->>>>>>> origin/monty
