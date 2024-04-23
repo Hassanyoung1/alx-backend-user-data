6e36bfdf381b34517a134b004094fe52fe60830b
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-04-23 13:49:36 +0100

    user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,3 +30,12 @@ class Auth:
             user = self._db.add_user(email, _hash_password(password))
             return user
         raise ValueError(f'User with email {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate a user by email and password"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
